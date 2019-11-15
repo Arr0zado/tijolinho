32fb1cef7697b0ae3c53a96ff27e6d7d4688ad75
Arr0zado <55147716+Arr0zado@users.noreply.github.com>
2019-11-15 23:45:43 +0000

    Update script.js

--- facial/script.js
+++ facial/script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets./tinyFaceDetector.loadFromUri('/facial/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/facial/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/facial/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/facial/models')
 ]).then(startVideo)
 
 function startVideo() {
